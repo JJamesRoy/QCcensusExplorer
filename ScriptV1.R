2adf94371d36229f0616c6ea3fa3cce7eb4654d2
JRCharlemagne <james22god@live.ca>
2022-09-22 16:41:20 -0400

    added R studio and cleaned tne names of the variables

--- ScriptV1.R
+++ ScriptV1.R
@@ -4,13 +4,21 @@ library(geojsonsf)
 library(leaflet)
 library(RColorBrewer)
 library(classInt)
+library(tidyverse)
+library(stringr)
 
 
 options(cancensus.api_key='CensusMapper_f622f3df6a9035ecbfee37004202c139')
 options(cancensus.cache_path = "C:/Users/james/OneDrive/Documents/R cache")
-dat_DA <- get_census(dataset='CA21', regions=list(CD=c("2466","2465")), vectors=c("v_CA21_1","v_CA21_4","v_CA21_6","v_CA21_386","v_CA21_434","v_CA21_452","v_CA21_497","v_CA21_498","v_CA21_593","v_CA21_596","v_CA21_671","v_CA21_719","v_CA21_767","v_CA21_905","v_CA21_1139","v_CA21_1144","v_CA21_906","v_CA21_907","v_CA21_923","v_CA21_944"), labels="detailed", geo_format="sf", level='DA')
-# dat_CT <- get_census(dataset='CA21', regions=list(CD=c("2466","2465")), vectors=c("v_CA21_1","v_CA21_4","v_CA21_6","v_CA21_386","v_CA21_434","v_CA21_452","v_CA21_497","v_CA21_498","v_CA21_593","v_CA21_596","v_CA21_671","v_CA21_719","v_CA21_767","v_CA21_905","v_CA21_1139","v_CA21_1144","v_CA21_906","v_CA21_907","v_CA21_923","v_CA21_944"), labels="detailed", geo_format="sf", level='CT')
+dat_DA <- get_census(dataset='CA21', regions=list(CD=c("2466","2465")), vectors=c("v_CA21_1","v_CA21_4","v_CA21_6","v_CA21_386","v_CA21_434","v_CA21_452","v_CA21_497","v_CA21_498","v_CA21_593","v_CA21_596","v_CA21_671","v_CA21_719","v_CA21_767","v_CA21_905","v_CA21_1140","v_CA21_1144","v_CA21_906","v_CA21_907","v_CA21_923","v_CA21_944"), labels="detailed", geo_format="sf", level='DA')
+# dat_CT <- get_census(dataset='CA21', regions=list(CD=c("2466","2465")), vectors=c("v_CA21_1","v_CA21_4","v_CA21_6","v_CA21_386","v_CA21_434","v_CA21_452","v_CA21_497","v_CA21_498","v_CA21_593","v_CA21_596","v_CA21_671","v_CA21_719","v_CA21_767","v_CA21_905","v_CA21_1140","v_CA21_1144","v_CA21_906","v_CA21_907","v_CA21_923","v_CA21_944"), labels="detailed", geo_format="sf", level='CT')
 
+
+dat_DA = dat_DA %>% 
+  rename_with(str_replace, pattern = "v_", replacement = "")
+
+dat_DA = dat_DA %>% 
+  rename_with(str_replace, pattern = ":.*", replacement = "")
 
 breaks_qt <- classIntervals(dat_DA$`v_CA21_906: Median total income of household in 2020 ($)`, n = 9, style = "quantile")
 #Break the variable into quantiles
