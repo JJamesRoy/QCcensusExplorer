62f599679d4f9677b1c80798f334b9d42dde7f8b
AlfredAssal <112986673+AlfredAssal@users.noreply.github.com>
2022-09-24 16:18:45 -0400

    Update app.R

--- app.R
+++ app.R
@@ -23,7 +23,7 @@ library(stringr)
 library(janitor)
 
 options(cancensus.api_key='CensusMapper_f622f3df6a9035ecbfee37004202c139')
-options(cancensus.cache_path = "C:/Users/Alfred/OneDrive/sciences_economiques/FAS1002/Projet spécial/cache")
+options(cancensus.cache_path = "C:/Users/Alfred/OneDrive/sciences_economiques/FAS1002/Projet_spécial/cache")
 dat_DA <- get_census(dataset='CA21', regions=list(CD=c("2466","2465")), vectors=c("v_CA21_1","v_CA21_4","v_CA21_6","v_CA21_386","v_CA21_434","v_CA21_452","v_CA21_497","v_CA21_498","v_CA21_593","v_CA21_596","v_CA21_671","v_CA21_719","v_CA21_767","v_CA21_905","v_CA21_1140","v_CA21_1144","v_CA21_906","v_CA21_907","v_CA21_923","v_CA21_944"), labels="detailed", geo_format="sf", level='DA')
 # dat_CT <- get_census(dataset='CA21', regions=list(CD=c("2466","2465")), vectors=c("v_CA21_1","v_CA21_4","v_CA21_6","v_CA21_386","v_CA21_434","v_CA21_452","v_CA21_497","v_CA21_498","v_CA21_593","v_CA21_596","v_CA21_671","v_CA21_719","v_CA21_767","v_CA21_905","v_CA21_1140","v_CA21_1144","v_CA21_906","v_CA21_907","v_CA21_923","v_CA21_944"), labels="detailed", geo_format="sf", level='CT')
 
