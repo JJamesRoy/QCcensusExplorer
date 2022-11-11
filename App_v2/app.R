3e6717ec0ae7a197c1b5d9f5c4d0e6d4272773ea
Alfred Assal <alfredassal@hotmail.ca>
2022-11-11 12:22:26 -0500

    update app

--- App_v2/app.R
+++ App_v2/app.R
@@ -18,7 +18,7 @@ library(ggplot2)
 library(scales)
 
 options(cancensus.api_key='CensusMapper_f622f3df6a9035ecbfee37004202c139')
-options(cancensus.cache_path = "C:/Users/james/OneDrive/Documents/R cache")
+options(cancensus.cache_path = "data/raw/")
 dat_DA <- get_census(dataset='CA21', regions=list(CD=c("2466","2465")), vectors=c("v_CA21_1","v_CA21_4","v_CA21_6","v_CA21_386","v_CA21_434","v_CA21_452","v_CA21_497","v_CA21_498","v_CA21_593","v_CA21_596","v_CA21_671","v_CA21_719","v_CA21_767","v_CA21_905","v_CA21_1140","v_CA21_1144","v_CA21_906","v_CA21_907","v_CA21_923","v_CA21_944"), labels="detailed", geo_format="sf", level='DA')
 # dat_CT <- get_census(dataset='CA21', regions=list(CD=c("2466","2465")), vectors=c("v_CA21_1","v_CA21_4","v_CA21_6","v_CA21_386","v_CA21_434","v_CA21_452","v_CA21_497","v_CA21_498","v_CA21_593","v_CA21_596","v_CA21_671","v_CA21_719","v_CA21_767","v_CA21_905","v_CA21_1140","v_CA21_1144","v_CA21_906","v_CA21_907","v_CA21_923","v_CA21_944"), labels="detailed", geo_format="sf", level='CT')
 
@@ -46,7 +46,7 @@ dat_DA = dat_DA %>%
 
 #testtest1 = dat_pc %>% nest(GeoUID)
 
-#dat = left_join(dat_DA, dat_pc, by = 'GeoUID') #Add the postal code as a vector ?
+#dat = left_join(dat_DA, dat_pc, by = 'GseoUID') #Add the postal code as a vector ?
 #test = merge(dat_DA, dat_pc, by = "GeoUID")
 ###
 
@@ -59,7 +59,7 @@ p_popup <- paste0("<strong>Revenu médian des ménages ($) : </strong>", dat_DA$
 
 
 #Enlève toutes les lignes non complète
-data_complete <- test[complete.cases(test$CA21_906), ]
+data_complete <- dat_DA[complete.cases(dat_DA$CA21_906), ]
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
